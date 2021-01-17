424dc5278c62d26e0eec78a4a8e6410ea732b76a
kaiweinan <kaiweinan@yahoo.com>
2021-01-16 23:13:55 -0500

    commit tree 394152e881684ce91174070dad97dbc7b86fbc3b

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
+  attr_reader :name
+attr_accessor :owner, :mood
+
+@@all=[]
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all<<self
+  end
   # code goes here
+  def self.all
+    @@all
+  end
 end
